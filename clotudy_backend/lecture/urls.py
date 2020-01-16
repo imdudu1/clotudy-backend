0d68943494af81c38d4581be95ac657dc7249801
kjuno95 <admin@mail.pppr.io>
2020-01-16 03:47:35 -0800

    수업 관리자 화면을 위한 템플릿 작성

--- clotudy_backend/lecture/urls.py
+++ clotudy_backend/lecture/urls.py
@@ -4,8 +4,8 @@ from .views import lecture, pdf_render, lecture_admin
 
 app_name = "lecture"
 urlpatterns = [
-    path("<int:room_id>/", view=lecture, name="view_lecture"),
-    path("pdf/", view=pdf_render, name="pdf_render"),
+    path("<int:room_id>/", view=lecture, name="lecture"),
+    path("pdf/", view=pdf_render, name="pdfViewer"),
 
-    path("test/<int:room_id>", view=lecture_admin, name="test"),
+    path("admin/<int:room_id>", view=lecture_admin, name="lectureAdminPage"),
 ]
