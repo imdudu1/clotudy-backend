f2f8f65446f18337a617fab9f04168dd8d965b2f
kjuno95 <admin@mail.pppr.io>
2020-01-18 04:54:15 -0800

    퀴즈를 위한 모델 생성과 수업 관련 모델 관계 및 컬럼명 수정

--- clotudy_backend/lecture/models.py
+++ clotudy_backend/lecture/models.py
@@ -6,21 +6,47 @@ class ClassInformation(models.Model):
     # 강의 제목
     class_title = models.CharField(max_length=50, blank=False)
     # 담당 교수 이름
-    class_advisor = models.CharField(max_length=20, blank=False)
+    class_instructor = models.CharField(max_length=20, blank=False)
     # 강의 소개
-    class_explain = models.TextField(max_length=512)
-    # 강의 종류
-    class_type = models.IntegerField(default=0)
+    class_description = models.TextField(max_length=512)
     # 생성일
     class_created_time = models.DateTimeField(default=timezone.now, blank=True)
     # 강의 초대 코드
-    class_invite_code = models.CharField(max_length=6, default="123")
-    class_pdf_path = models.CharField(max_length=50, blank=False)
+    class_invite_code = models.CharField(max_length=6, default="111111")
+
+
+class LectureInformation(models.Model):
+    class_info = models.ForeignKey(ClassInformation, on_delete=models.CASCADE)
+    lecture_title = models.CharField(max_length=50)
+    lecture_type = models.IntegerField(default=0)
+    lecture_rsrc_path = models.CharField(max_length=256)
+
+
+class QuizBox(models.Model):
+    lecture_info = models.ForeignKey(LectureInformation, on_delete=models.CASCADE)
+    quiz_box_title = models.CharField(max_length=30)
+
+
+class Quiz(models.Model):
+    quiz_box_info = models.ForeignKey(QuizBox, on_delete=models.CASCADE)
+    quiz_prob = models.TextField(max_length=512)
+    # 0: choice 1: essay
+    quiz_type = models.IntegerField(default=0)
+    quiz_item_num = models.IntegerField(default=5)
+    quiz_score = models.IntegerField(default=1)
+    quiz_solve_count = models.IntegerField(default=0)
+
+
+class Answer(models.Model):
+    quiz_info = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    answer_content = models.CharField(max_length=20)
+    answer_choice_count = models.IntegerField(default=0)
+    answer_is_correct = models.BooleanField(default=False)
 
 
 class QuestionMessage(models.Model):
     # 1:N 모양을 위한 ClassInformation 와 ForeignKey 설정
-    class_info = models.ForeignKey(ClassInformation, on_delete=models.CASCADE)
+    class_info = models.ForeignKey(LectureInformation, on_delete=models.CASCADE)
     # 생성일
     created_time = models.DateTimeField(default=timezone.now)
     # 질문 내용
