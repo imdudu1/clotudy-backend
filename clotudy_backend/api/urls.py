8acc2fa6b3753b3508ca8495260cc477f609ad84
kjuno95 <admin@mail.pppr.io>
2020-01-21 00:19:31 -0800

    퀴즈 정보를 가져오기 위한 RestAPI 작성

--- clotudy_backend/api/urls.py
+++ clotudy_backend/api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import QuizBoxDetail
 
 app_name = "api"
 urlpatterns = [
+    path("quiz/<int:pk>/", QuizBoxDetail.as_view(), name="quizList"),
 ]
--- clotudy_backend/api/views.py
+++ clotudy_backend/api/views.py
@@ -1,1 +1,19 @@
+from django.http import Http404
 from django.shortcuts import render
+from clotudy_backend.lecture.models import QuizBox, Quiz, Answer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class QuizBoxDetail(APIView):
+    def get(self, request, pk, format=None):
+        quiz_box = QuizBox.objects.get(lecture_info=pk)
+        if quiz_box.quiz_is_open == True:
+            quiz_set = {"category_id": quiz_box.pk, "is_open": quiz_box.quiz_is_open,
+                        "category_title": quiz_box.quiz_box_title, "quiz_content": []}
+            quiz_list = Quiz.objects.filter(quiz_box_info=quiz_box.pk)
+            for quiz in quiz_list:
+                answer_list = Answer.objects.filter(quiz_info=quiz.pk)
+                quiz_set["quiz_content"].append({"id": quiz.pk, "problem": quiz.quiz_prob, "answer": [
+                    {"id": answer.pk, "content": answer.answer_content} for answer in answer_list]})
+        return Response(quiz_set)
