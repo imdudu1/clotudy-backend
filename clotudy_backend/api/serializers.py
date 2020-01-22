4ac7ca6a8842a6e3aa9a09b19743559a42e2b737
kjuno95 <admin@mail.pppr.io>
2020-01-21 19:21:10 -0800

    Git 설정 및 정적파일 추가

--- clotudy_backend/api/serializers.py
+++ clotudy_backend/api/serializers.py
@@ -1,2 +1,8 @@
 from rest_framework import serializers
+from clotudy_backend.lecture.models import QuizBox, Quiz, Answer
 
+
+class QuizBoxSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QuizBox
+        fields = ('quiz_box_title', 'quiz_is_open')
