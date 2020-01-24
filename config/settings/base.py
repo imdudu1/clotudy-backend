e7e6a5868e8021944d1e57e022810c83b9ba22c8
kjuno95 <admin@mail.pppr.io>
2020-01-24 06:28:33 -0800

    이메일 인증과정 생략

--- config/settings/base.py
+++ config/settings/base.py
@@ -300,7 +300,7 @@ ACCOUNT_AUTHENTICATION_METHOD = "username"
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
 ACCOUNT_EMAIL_REQUIRED = True
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
-ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+ACCOUNT_EMAIL_VERIFICATION = "none" #"mandatory"
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
 ACCOUNT_ADAPTER = "clotudy_backend.users.adapters.AccountAdapter"
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
