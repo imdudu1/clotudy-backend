6ddfde9f0d28587e566fa8ba944b8fd9121a77ce
kjuno95 <admin@mail.pppr.io>
2020-02-10 20:10:15 -0800

    lp

---
+++ installer.sh
@@ -0,0 +1,13 @@
+export CELERY_BROKER_URL=redis://localhost:6379/0
+
+sudo apt-get install -y python3 python3-pip python3-dev redis-server pgadmin3 postgresql postgresql-contrib gcc g++ git wget curl vim libpq-dev yarn
+
+
+# sudo -i -u postgres
+# psql
+# ALTER USER postgres WITH PASSWORD 'new_password';
+# \q
+
+# sudo -u postgres createuser $USER
+# sudo -u postgres createdb clotudy_backend -O $USER
+
