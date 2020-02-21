ddf2907bb33f44fba88fe4cb8a5346287374180b
kjuno95 <admin@mail.pppr.io>
2020-02-21 21:06:54 +0900

    멀니;어리ㅏㅁ넝ㄹ

--- clotudy_backend/api/views.py
+++ clotudy_backend/api/views.py
@@ -56,17 +56,6 @@ class QuizBoxDetail(APIView):
 
 class PPTTimeHistory(APIView):
 
-    def get(self, request, pk, format=None):
-        if request.user.is_authenticated:
-            lecture = get_object_or_404(LectureInformation, pk=pk)
-            times = lecture.lecture_ppt_times.split(';')[:-1]
-            if len(times) > 0:
-                res = []
-                for time in times:
-                    res.append(int(time))
-                return Response(res)
-        return Response([])
-
     def post(self, request, pk, format=None):
         if request.user.is_authenticated:
             lecture = get_object_or_404(LectureInformation, pk=pk)
--- clotudy_backend/lecture/views.py
+++ clotudy_backend/lecture/views.py
@@ -21,7 +21,7 @@ def lecture(request, class_id, lecture_id):
                         "lecture_type": lecture_info.lecture_type,
                         "lecture_note": lecture_info.lecture_note}
 
-        times = lecture_info.lecture_ppt_times.split(';')[:-1]
+        times = lecture_info.lecture_ppt_times.split(';')
         time_list = []
         if len(times) > 0:
             for time in times:
@@ -55,8 +55,14 @@ def lecture_admin(request, class_info, lecture_id):
                  "is_correct": answer.answer_is_correct} for answer in answer_list]})
         recv_quiz_data.append(quiz_set)
 
+    times = lecture_info.lecture_ppt_times.split(';')
+    time_list = []
+    if len(times) > 0:
+        for time in times:
+            time_list.append(int(time))
+
     return render(request, 'lecture/admin/{}'.format(_get_template_html_name(lecture_info.lecture_type)), {
-        'room_name_json': mark_safe(json.dumps(class_info.pk)),
+        'room_name_json': mark_safe(json.dumps(lecture_id)),
         'quiz_data': recv_quiz_data,
         'questions': _get_user_questions_from_db(lecture_id),
     })
