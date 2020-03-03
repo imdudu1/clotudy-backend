2551ad53a522d2e044304d3a0a5c098b15f11fa9
V3lk0z <kr.revolt@gmail.com>
2020-03-04 02:01:03 +0900

    api URL 변경 및 class_pk별로 분기

--- clotudy_backend/api/urls.py
+++ clotudy_backend/api/urls.py
@@ -3,6 +3,6 @@ from .views import QuizBoxDetail, PPTTimeHistory
 
 app_name = "api"
 urlpatterns = [
-    path("quiz/<int:pk>", QuizBoxDetail.as_view(), name="quizList"),
+    path("quiz/<int:class_pk>/<int:lecture_pk>", QuizBoxDetail.as_view(), name="quizList"),
     path("ppthistory/<int:pk>", PPTTimeHistory.as_view(), name="pptTime"),
 ]
--- clotudy_backend/api/views.py
+++ clotudy_backend/api/views.py
@@ -1,35 +1,44 @@
 from django.http import Http404
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
-from clotudy_backend.lecture.models import QuizBox, Quiz, Answer, QuizScoreRecord, LectureInformation
+from clotudy_backend.lecture.models import QuizBox, Quiz, Answer, QuizScoreRecord, LectureInformation, ClassInformation
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 import json
 
 class QuizBoxDetail(APIView):
+    authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
 
     def check_login(self, request):
         # TODO: Check if member is subscribed to the meeting
         return request.user.is_authenticated
 
-    def get(self, request, pk, format=None):
+    def get(self, request, class_pk, lecture_pk, format=None):
         if self.check_login(request):
-            quiz_box = get_object_or_404(QuizBox, lecture_info=pk)
+            quiz_box = get_object_or_404(QuizBox, lecture_info=lecture_pk)
             if quiz_box.quiz_is_open:
+                class_info = get_object_or_404(ClassInformation, pk=class_pk)
                 quiz_set = {"category_id": quiz_box.pk, "is_open": quiz_box.quiz_is_open,
                             "category_title": quiz_box.quiz_box_title, "quiz_content": []}
-                quiz_list = Quiz.objects.filter(quiz_box_info=quiz_box)
-                for quiz in quiz_list:
-                    answer_list = Answer.objects.filter(quiz_info=quiz)
-                    quiz_set["quiz_content"].append({"id": quiz.pk, "problem": quiz.quiz_prob, "answer": [
-                        {"id": answer.pk, "content": answer.answer_content} for answer in answer_list]})
+                if class_info.class_instructor_id == request.user.username:
+                    quiz_list = Quiz.objects.filter(quiz_box_info=quiz_box)
+                    for quiz in quiz_list:
+                        answer_list = Answer.objects.filter(quiz_info=quiz)
+                        quiz_set["quiz_content"].append({"id": quiz.pk, "problem": quiz.quiz_prob, "answer": [
+                            {"id": answer.pk, "content": answer.answer_content} for answer in answer_list]})
+                else:
+                    quiz_list = Quiz.objects.filter(quiz_box_info=quiz_box)
+                    for quiz in quiz_list:
+                        answer_list = Answer.objects.filter(quiz_info=quiz)
+                        quiz_set["quiz_content"].append({"id": quiz.pk, "problem": quiz.quiz_prob, "answer": [
+                            {"id": answer.pk, "content": answer.answer_content} for answer in answer_list]})
                 return Response([quiz_set])
         return Response([])
 
-    def post(self, request, pk, format=None):
+    def post(self, request, class_pk, lecture_pk, format=None):
         if self.check_login(request):
-            quiz_box = get_object_or_404(QuizBox, pk=pk)
+            quiz_box = get_object_or_404(QuizBox, pk=lecture_pk)
             if quiz_box.quiz_is_open:
                 try:
                     record = QuizScoreRecord.objects.get(quiz_box_info=quiz_box, user_id=request.user.username)
