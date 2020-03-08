baec8e6f5579a37bf676a73903c99d2b06a74345
V3lk0z <kr.revolt@gmail.com>
2020-03-08 15:20:23 +0900

    API를 위한 URL 라우팅

--- clotudy_backend/api/urls.py
+++ clotudy_backend/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import QuizBoxDetail, PPTTimeHistory
+from .views import QuizBoxDetail, PPTTimeHistory, BonusPoint
 
 app_name = "api"
 urlpatterns = [
     path("quiz/<int:class_pk>/<int:lecture_pk>", QuizBoxDetail.as_view(), name="quizList"),
     path("ppthistory/<int:pk>", PPTTimeHistory.as_view(), name="pptTime"),
+    path("bonus/<int:class_pk>/<int:lecture_pk>", BonusPoint.as_view(), name="bonus"),
 ]
